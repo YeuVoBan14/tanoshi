8a1131b5e658d39631aaaf59fe37d4e6a39a7454
YeuVoBan14 <nhathung142002@gmail.com>
2024-12-03 12:51:05 +0700

    hung

--- site1/site1/settings.py
+++ site1/site1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-oqg)(jv1!vgd4$1!76j+y8vqx0#=%ii1w5g6)@70oc@y6m0zhc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'site1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',  # Đường dẫn đến file SQLite
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'site1',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
     }
 }
 
@@ -106,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Bangkok'
 
 USE_I18N = True
 
